46478e9baa45df29fbb494c6a10570e932cf3bc8
hsj7768 <hsj7768@naver.com>
2020-02-29 02:53:41 +0900

    Update result.js

--- js/result.js
+++ js/result.js
@@ -26,23 +26,28 @@ $(document).ready(function () {
   }
     
   
-  getCSV( function() {
+  getCSV( function(data) {
+    items = data
+
     if ( listed && listed.length > 0 ) {
       searchApp.listed = listed;
       searchApp.hide = true;
       searchApp.searchDetail();
-      
+
     } else if ( search ) {
       searchApp.searchDetail();
-      
+
     } else if ( parsed.cart ) {
       searchApp.listed = listApp.carts;
       listApp.cartmode = true;
       searchApp.hide = true;
       searchApp.searchDetail();
+
+    } else {
+      searchApp.searchDetail();
     }
   });
-  
+
   $(window).scroll(function() {
     var docHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
@@ -100,37 +105,12 @@ function getAllUrlParams( url ) {
   return obj;
 }
 
-function getCSV( callback ) {
-  /*
-  var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTWze7qOfMnYJC-KZBlSmLpmut8hARDX4zS_rAbRraECdgQ7GDVu5wOvqIu7uUrksyZoEzwH3Lt5K2K/pub?gid=562347642&single=true&output=csv"
-  $.get( url, function( data, status ) {
-    var json = $.csv.toObjects( data );    
-    items = json;
-    callback();
-  });
-  */
-  items = data;
-  callback();
-}
-
 
 function setItems( filtered ) {
   listApp.scrolls = [];
   listApp.scrollsCurrent = 0;
   listApp.items = filtered;
-  
-  /*
-  var url = "";
-  if ( filtered.length > 0 ) {
-    url += "?listed=";
-    for ( i in filtered ) {
-      url += filtered[i].id + ',';
-    }
-    url = url.substring(0, url.length - 1);
-  }
-  
-  history.pushState(null, null, url);
-  */
+
   loadMore();
 }
 
@@ -162,7 +142,7 @@ function checkbox( e ) {
   var app = ids[0];
   var group = ids[1];
   var id = ids[2];
-  
+
   if ( app == 'search' ) {
     searchApp[ group ][id] = !searchApp[ group ][id];
   } else if ( app == 'result' ) {
@@ -175,7 +155,7 @@ function checkedbox( e ) {
   var app = ids[0];
   var group = ids[1];
   var id = ids[2];
-  
+
   if ( app == 'search' ) {
     searchApp[ group ][id].checked = !searchApp[ group ][id].checked;
   } else if ( app == 'search' ) {
@@ -183,7 +163,7 @@ function checkedbox( e ) {
   }
 }
 
-function clearSiblings( app, group ) {  
+function clearSiblings( app, group ) {
   var obj = ( app == 'search' ? searchApp[ group ] : app == 'result' ? listApp[ group ] : '' );
   for (var key in obj ) {
     if (obj.hasOwnProperty(key)) {
@@ -234,13 +214,13 @@ var listApp = new Vue({
     }
   }
   ,methods: {
-    checkCart( id ) {      
+    checkCart( id ) {
       var cookie = Cookies.get('cart');
       if ( cookie != null ) {
         var cart = JSON.parse( cookie );
         return cart.includes ( id*1 );
       }
-      
+
       return false;
     }
     ,carting( id ) {
@@ -251,16 +231,16 @@ var listApp = new Vue({
       } else {
         cart = JSON.parse( cookie );
       }
-      
+
       if ( this.checkCart( id ) ) {
         cart.splice( cart.indexOf( id*1 ), 1 );
       } else {
         cart.push (id*1);
       }
-      
+
       this.carts = cart;
       Cookies.set( 'cart', cart, { expires: 7 } );
-      
+
       if ( this.cartmode ) {
         var filted = this.items.filter( function( item ) {
           if ( cart.includes( item.id*1 ) ) {
@@ -269,7 +249,7 @@ var listApp = new Vue({
         });
         setItems( filted );
       }
-      
+
       createAlert('장바구니','','장바구니는 7일간 유효합니다','info',false,true,'pageMessages');
     }
     ,sortOption : function( sorttype ) {
@@ -277,7 +257,7 @@ var listApp = new Vue({
       if ( sorttype == null ) {
         sorttype = getRadio( this['sorttype'] );
       }
-      
+
       // 정렬 필드
       if ( sorttype == 'price' ) {
         sorted.sort(function(a, b) {
@@ -292,7 +272,7 @@ var listApp = new Vue({
             return a['promotion_discount'] - b['promotion_discount'];
         });
       }
-      
+
       setItems( sorted );
     }
   }
@@ -308,214 +288,7 @@ var searchApp = new Vue({
     ,hide: false
     ,listed: [
     ]
-    ,search: {
-      simple : [
-        {
-          label: '음성 무제한',
-          id: 'infinity',
-          checked: false,
-        },
-        {
-          label: '1만원 이하',
-          id: 'cheap',
-          checked: false,
-        },        
-        {
-          label: '프로모션',
-          id: 'event',
-          checked: false,
-        },
-        {
-          label: '데이터 무제한',
-          id: 'data',
-          checked: false,
-        },
-        {
-          label: '제휴카드',
-          id: 'card',
-          checked: false,
-        }
-      ]
-      ,agency : [
-        {
-          label: 'SKT',
-          id: 'skt',
-          checked: false,
-        },
-        {
-          label: 'KT',
-          id: 'kt',
-          checked: false,
-        },
-        {
-          label: 'LG U+',
-          id: 'lg',
-          checked: false,
-        }
-      ]
-      ,network : [
-        {
-          label: '3G',
-          id: '3g',
-          checked: false
-        },
-        {
-          label: 'LTE',
-          id: 'lte',
-          checked: false
-        }
-      ]
-      ,data : [
-        {
-          label: '데이터 없음',
-          field:'data',
-          from: 0,
-          to: 0,
-          checked: false
-        },
-        {
-          label: '300M이하',
-          field:'data',
-          from: 1,
-          to: 300,
-          checked: false
-        },
-        {
-          label: '500M이하',
-          field:'data',
-          from: 301,
-          to: 500,
-          checked: false
-        },
-        {
-          label: '750M이하',
-          field:'data',
-          from: 501,
-          to: 750,
-          checked: false
-        },
-        {
-          label: '1G이하',
-          field:'data',
-          from: 751,
-          to: 1024,
-          checked: false
-        },
-        {
-          label: '1.5G이하',
-          field:'data',
-          from: 1025,
-          to: 1536,
-          checked: false
-        },
-        {
-          label: '2G이하',
-          field:'data',
-          from: 1537,
-          to: 2048,
-          checked: false
-        },
-        {
-          label: '3G이하',
-          field:'data',
-          from: 2049,
-          to: 3072,
-          checked: false
-        },
-        {
-          label: '6G이하',
-          field:'data',
-          from: 3073,
-          to: 6144,
-          checked: false
-        },
-        {
-          label: '10G이하',
-          field:'data',
-          from: 6145,
-          to: 10240,
-          checked: false
-        },
-        {
-          label: '11G이상',
-          field:'data',
-          from: 10241,
-          to: 1024000,
-          checked: false
-        }
-      ]
-      ,datainfinity : [
-        {
-          label: '매일 1~2G 제공',
-          field:'data_daily_offer',
-          matching:['1G', '2G'],
-          checked: false
-        },
-        {
-          label: '소진 후 2~5Mbps',
-          field:'data_infinity',
-          matching:['2Mbps', '3Mbps', '5Mbps'],
-          checked: false
-        },
-        {
-          label: '소진 후 2~400Kbps',
-          field:'data_infinity',
-          matching:['200Kbps', '400Kbps'],
-          checked: false
-        }
-      ]
-      ,call : [
-        {
-          label: '통화 없음',
-          field:'call',
-          from: 0,
-          to: 0,
-          checked: false
-        },
-        {
-          label: '60분이하',
-          field:'call',
-          from: 1,
-          to: 60,
-          checked: false
-        },
-        {
-          label: '100분이하',
-          field:'call',
-          from: 61,
-          to: 100,
-          checked: false
-        },
-        {
-          label: '180분이하',
-          field:'call',
-          from: 101,
-          to: 180,
-          checked: false
-        },
-        {
-          label: '300분이하',
-          field:'call',
-          from: 181,
-          to: 300,
-          checked: false
-        },
-        {
-          label: '330분이상',
-          field:'call',
-          from: 301,
-          to: 10000,
-          checked: false
-        },
-        {
-          label: '무제한',
-          field:'call',
-          from: -1,
-          to: -1,
-          checked: false
-        }
-      ]
-   }
+    ,search: searchOptions
   }
   ,methods: {
     searchChecked( item ) {
@@ -524,9 +297,9 @@ var searchApp = new Vue({
     }
     ,searchDetail() {
       var filted = items;
-      
+
       var listed = this.listed;
-      if ( listed && listed.length > 0 ) { 
+      if ( listed && listed.length > 0 ) {
         filted = filted.filter( function( item ) {
           if ( listed.includes( item.id*1 ) ) {
             return true;
@@ -535,74 +308,70 @@ var searchApp = new Vue({
         setItems( filted );
         return;
       }
-      
+
       var simples = this.search.simple;
-      
-      if ( simples[0].id = 'infinity' && simples[0].checked ) {
-        filted = filted.filter( function( item ) {
-            if ( item.call < 0 ) {
-              return true;
-            }
-        });
-      } 
-      
-      if ( simples[1].id = 'cheap' && simples[1].checked ) {
-        filted = filted.filter( function( item ) {
-            if ( item.price <= 10000 ) {
-              return true;
-            }
-        });
-      }
-      
-      if ( simples[2].id = 'event' && simples[2].checked ) {
-        filted = filted.filter( function( item ) {
-            if ( item.promotion ) {
-              return true;
-            }
-        });
-      }
-      
-      if ( simples[3].id = 'data' && simples[3].checked ) {
-        filted = filted.filter( function( item ) {
-            if ( item.data_daily_offer ) {
-              return true;
-            } else if ( item.data_infinity ) {
-              return true;
-            }
-        });
-      }
-      
-      if ( simples[4].id = 'card' && simples[4].checked ) {
-        filted = filted.filter( function( item ) {
-            if ( item.card1 ) {
-              return true;
-            } else if ( item.data_infinity ) {
-              return true;
-            }
-        });
-      }
-      
+
+      simples.forEach(function(simple) {
+        if (simple.checked == false)
+            return;
+
+        if (simple.id = 'infinity') {
+            filted = filted.filter( function( item ) {
+                if ( item.call_amount < 0 ) return true;
+            });
+
+        } else if (simple.id = 'cheap') {
+            filted = filted.filter( function( item ) {
+                if ( item.price <= 10000 )
+                  return true;
+            })
+
+        } else if (simple.id = 'event') {
+            filted = filted.filter( function( item ) {
+                if ( item.promotion )
+                  return true;
+            })
+
+        } else if (simple.id = 'data') {
+            filted = filted.filter( function( item ) {
+                if ( item.data_daily_offer )
+                  return true;
+                else if ( item.data_infinity )
+                  return true;
+            })
+
+        } else if (simple.id = 'card'   ) {
+            filted = filted.filter( function( item ) {
+                if ( item.card1 )
+                  return true;
+                else if ( item.data_infinity )
+                  return true;
+            })
+        }
+      })
+
+
       var agency = getCheckbox( this.search['agency'] );
       var network = getCheckbox( this.search['network'] );
       var data = getCheckbox( this.search['data'] );
       var datainfinity = getCheckbox( this.search['datainfinity'] );
       var call = getCheckbox( this.search['call'] );
-      
+
       /*
       // 이름으로 필터링
       filted = filted.filter( function ( item ) {
         return item.title == '이야기 데이터 11GB';
       });
       */
-      
-      if ( agency && agency.length > 0 ) { 
+
+      if ( agency && agency.length > 0 ) {
         filted = filted.filter( function( item ) {
           for ( var i in agency ) {
             return item.agency.toUpperCase() == agency[i].id.toUpperCase();
           }
         });
       }
-      
+
       if ( network && network.length > 0 ) {
         filted = filted.filter( function( item ) {
           if ( item.network == '3G/LTE' ) {
@@ -615,8 +384,8 @@ var searchApp = new Vue({
           }
         });
       }
-      
-      if ( data && data.length > 0 ) { 
+
+      if ( data && data.length > 0 ) {
         filted = filted.filter( function( item ) {
           for (var i in data) {
             if ( item.data_total >= data[i].from && item.data_total <= data[i].to ) {
@@ -625,7 +394,7 @@ var searchApp = new Vue({
           }
         });
       }
-      
+
       if ( datainfinity != null && datainfinity.length > 0 ) {
         filted = filted.filter( function( item ) {
           for (var i in datainfinity) {
@@ -638,17 +407,17 @@ var searchApp = new Vue({
           }
         });
       }
-      
-      if ( call && call.length > 0 ) { 
+
+      if ( call && call.length > 0 ) {
         filted = filted.filter( function( item ) {
           for (var i in call) {
-            if ( item.call >= call[i].from && item.call <= call[i].to ) {
+            if ( item.call_amount >= call[i].from && item.call_amount <= call[i].to ) {
               return true;
             }
           }
         });
       }
-            
+
       setItems( filted );
     }
   }
@@ -684,7 +453,7 @@ function createAlert(title, summary, details, severity, dismissible, autoDismiss
     var msgTitle = $("<h4 />", {
       html: title
     }).appendTo(msg);
-    
+
     if(!iconAdded){
       msgTitle.prepend(msgIcon);
       iconAdded = true;
@@ -695,7 +464,7 @@ function createAlert(title, summary, details, severity, dismissible, autoDismiss
     var msgSummary = $("<strong />", {
       html: summary
     }).appendTo(msg);
-    
+
     if(!iconAdded){
       msgSummary.prepend(msgIcon);
       iconAdded = true;
@@ -706,13 +475,13 @@ function createAlert(title, summary, details, severity, dismissible, autoDismiss
     var msgDetails = $("<p />", {
       html: details
     }).appendTo(msg);
-    
+
     if(!iconAdded){
       msgDetails.prepend(msgIcon);
       iconAdded = true;
     }
   }
-  
+
 
   if (dismissible) {
     var msgClose = $("<span />", {
@@ -721,9 +490,9 @@ function createAlert(title, summary, details, severity, dismissible, autoDismiss
       html: "<i class='fa fa-times-circle'></i>"
     }).appendTo(msg);
   }
-  
+
   $('#' + appendToId).prepend(msg);
-  
+
   if(autoDismiss){
     setTimeout(function() {
       msg.addClass("flipOutX");
